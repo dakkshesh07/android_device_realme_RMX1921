259238cb391159f6202df1e0830b11d57f3f5cde
shashank1436 <9945shashank@gmail.com>
2021-01-16 08:04:52 +0100

    RMX1971: Enable logcat service
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Signed-off-by: dakkshesh <dakkshesh5@gmail.com>

--- device.mk
+++ device.mk
@@ -403,7 +403,8 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc \
-    apex_metadata.rc
+    apex_metadata.rc \
+    logcat.rc
 
 # Recovery
 PRODUCT_PACKAGES += \

842c437f1724ddd4f68bdb1f2496a28aa6da89d3
Deepak5310 <deepakjangir5310@gmail.com>
2021-06-22 09:24:02 +0000

    RMX1921: Compile with Proton Clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,17 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright 2021 Project 404
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 DEVICE_PATH := device/realme/RMX1921
@@ -43,8 +53,16 @@ BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/realme/RMX1921
 TARGET_KERNEL_CONFIG := RMX1921_defconfig
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_USE_LATEST_CLANG := true
 TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
+TARGET_KERNEL_ADDITIONAL_FLAGS += AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_CROSS_COMPILE_ARM32 := arm-linux-gnueabi-
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm710

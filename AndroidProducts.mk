ed61ea78a36cc5852e75295c6e10a2bb0165dd1f
Dakkshesh <dakkshesh5@gmail.com>
2021-07-10 11:34:26 +0530

    AndroidProducts: re-arange build types

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/spark_RMX1921.mk
 
 COMMON_LUNCH_CHOICES := \
+    spark_RMX1921-user \
     spark_RMX1921-userdebug \
-    spark_RMX1921-eng \
-    spark_RMX1921-user
+    spark_RMX1921-eng

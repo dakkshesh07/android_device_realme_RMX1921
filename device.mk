50d724d4d174203160de4f958a60b0911a836f5e
Dakkshesh <dakkshesh5@gmail.com>
2021-08-16 20:52:52 +0530

    RMX1921: add device specific init script
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- device.mk
+++ device.mk
@@ -184,6 +184,11 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# Init scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.device.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.device.rc
+
+
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml

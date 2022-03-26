7b524e246f1e6061b65b4a9e64f6f771a4985a5d
Chenyang Zhong <zhongcy95@gmail.com>
2022-03-26 01:13:20 -0400

    RMX1921: switch to QtiMapperExtensions
    Those mapper and allocator vintfs are included as fragments in
    the display HAL. Drop them from manifest.
    
    Note: this needs my backports of QtiMapperExtensions in sdm845's
    display HAL.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- device.mk
+++ device.mk
@@ -158,7 +158,7 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
-    vendor.qti.hardware.display.allocator@1.0-service \
+    vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \

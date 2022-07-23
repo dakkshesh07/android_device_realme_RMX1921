ee55ad9543bc45d4fdf2c6bed6ab7b4e1e43c638
LuK1337 <priv.luk@gmail.com>
2022-07-23 15:05:33 +0530

    nicky: Build libssrec from source

--- device.mk
+++ device.mk
@@ -87,6 +87,7 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
+    libssrec \
     libtinycompress \
     libvolumelistener \
     tinymix

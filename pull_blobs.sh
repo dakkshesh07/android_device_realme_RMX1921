87171178a18932dec4c4c57e20845578e83fb38c
pjgowtham <pjgowtham@gmail.com>
2022-07-25 20:50:36 +0530

    nicky: Cleanup some oplus services

---
+++ pull_blobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+./extract-files.sh /home/pjgowtham/Documents/dumpyara/working/RMX1971_f06
+./extract-files.sh --kang --section "(System)" /home/pjgowtham/Documents/dumpyara/working/proprietary_vendor_qcom_common-sapphire/system/telephony/proprietary

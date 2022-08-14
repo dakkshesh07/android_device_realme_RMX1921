638ffaa05102e4514eaf8a8106470a07cfd3424e
pjgowtham <pjgowtham@gmail.com>
2022-08-14 13:17:55 +0530

    nicky: Dont build audioadsprpcd
    * None of the SDM710/SDM670/SDM845 targerts build this

--- device.mk
+++ device.mk
@@ -79,7 +79,6 @@ PRODUCT_PACKAGES += \
     audio.primary.sdm710 \
     audio.r_submix.default \
     audio.usb.default \
-    audioadsprpcd \
     liba2dpoffload \
     libbatterylistener \
     libcomprcapture \

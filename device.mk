5142ae1dd98ee0fe73ba0270620928370e2afdb0
Dakkshesh <dakkshesh5@gmail.com>
2023-06-11 21:22:07 +0530

    RMX1921: Nuke livedisplay
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- device.mk
+++ device.mk
@@ -253,10 +253,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.realme_sdm710
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libminijail \

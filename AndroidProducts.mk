7392d0c60185f9cd6d2dfd1bf111be75ee2e06de
Dakkshesh <dakkshesh5@gmail.com>
2023-06-25 12:37:00 +0530

    RMX1921: evox -> yaap
    Bye cringe.
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_RMX1921.mk
+    $(LOCAL_DIR)/yaap_RMX1921.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_RMX1921-userdebug \
-    evolution_RMX1921-eng
+    yaap_RMX1921-userdebug \
+    yaap_RMX1921-eng

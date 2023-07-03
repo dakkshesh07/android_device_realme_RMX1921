67af8e1f2bb66430d31b37b676140f0db290840e
Dakkshesh <dakkshesh5@gmail.com>
2023-07-03 13:22:39 +0000

    RMX1921: Build qti.hardware.display.mapper@1.1 and qti.hardware.display.mapperextensions
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- device.mk
+++ device.mk
@@ -152,9 +152,12 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator@1.0-service \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.display.mapperextensions@1.0 \
+    vendor.qti.hardware.display.mapperextensions@1.1
 
 # Dexpreopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \

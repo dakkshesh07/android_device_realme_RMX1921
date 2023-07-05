39f5b9d19e8267be09a5902e2a01e5b6a7b53c86
Dakkshesh <dakkshesh5@gmail.com>
2023-07-05 20:06:17 +0530

    RMX1921: EvoX -> PixelOS
    Bye cringe. Also manually override build fingerprint and set PRODUCT_SYSTEM_MODEL while on it.
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- aosp_RMX1921.mk
+++ aosp_RMX1921.mk
@@ -4,23 +4,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Evolution X stuff
-TARGET_BOOT_ANIMATION_RES := 1080
-EXTRA_UDFPS_ANIMATIONS := true
+# Inherit some common PixelOS stuff
 TARGET_SUPPORTS_QUICK_TAP := true
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+PRODUCT_NAME := aosp_RMX1921
 PRODUCT_BRAND := realme
-PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := evolution_RMX1921
+PRODUCT_DEVICE := RMX1921
 PRODUCT_MODEL := RMX1921
 
 PRODUCT_SYSTEM_NAME := RMX1921
 PRODUCT_SYSTEM_DEVICE := RMX1921
+PRODUCT_SYSTEM_MODEL := RMX1921
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
@@ -31,3 +30,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := realme/RMX1921/RMX1921:11/RKQ1.201217.002/1651205774457:user/release-keys
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)

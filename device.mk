183c01912e6d071bf17f4f4bbfeaaa43f6d85d24
Dakkshesh <dakkshesh5@gmail.com>
2023-07-12 19:38:27 +0530

    RMX1921: configs: Update priv app permission xmls from LA.QSSI.13.0.r1-10400-qssi.0
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- device.mk
+++ device.mk
@@ -443,7 +443,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/qti_whitelist_system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist_system_ext.xml \
-    $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti-system-ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti-system-ext.xml
 
 # Radio
 PRODUCT_PACKAGES += \

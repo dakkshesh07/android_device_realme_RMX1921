60b90867ac88824b42e67ce2e7acd30b5732706c
Dakkshesh <dakkshesh5@gmail.com>
2023-07-12 17:56:45 +0530

    RMX1921: Create vendorsetup.sh
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+# Vendor
+git clone https://git.n64.cc/dakkshesh07/vendor_realme_RMX1921.git -b 13 vendor/realme/RMX1921
+
+# Kernel
+git clone https://github.com/dakkshesh07/parallax_kernel_realme_RMX1921.git -b tiramisu kernel/realme/parallax-RMX1921
+
+# Qcom Common (AOSPA)
+git clone https://github.com/AOSPA/android_device_qcom_common.git -b topaz device/qcom/common
+git clone https://github.com/ThankYouMario/proprietary_vendor_qcom_common.git -b topaz vendor/qcom/common

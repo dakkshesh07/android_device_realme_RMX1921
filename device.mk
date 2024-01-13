42d7600748b387fa9d5a9f05fdb75db96e5e03db
Dakkshesh <dakkshesh5@gmail.com>
2024-01-13 17:13:18 +0530

    RMX1921: Build android.hardware.audio.sounddose-vendor-impl
    01-13 15:43:12.825  1050  1050 E audiohalservice: Failed to dlopen android.hardware.audio.sounddose-vendor-impl.so: dlopen failed: library "android.hardware.audio.sounddose-vendor-impl.so" not found
    01-13 15:43:12.825  1050  1050 W audiohalservice: createISoundDoseFactory() from android.hardware.audio.sounddose-vendor-impl failed
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- device.mk
+++ device.mk
@@ -56,6 +56,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.3-impl \
+    android.hardware.audio.sounddose-vendor-impl \
     audio.primary.sdm710 \
     audio.r_submix.default \
     audio.usb.default \
